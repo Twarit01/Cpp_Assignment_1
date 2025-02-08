3d20f327cdb8df7c1826f65cfebee05789ec56fc
Twarit01 <twaritshukla.01@gmail.com>
2025-02-08 23:37:50 +0530

    Initial commit

--- Q3_B.cpp
+++ Q3_B.cpp
@@ -1,1 +1,20 @@
- 
+//Checks whether the string is palindrome
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    getline(cin, str);
+    int left = 0, right = str.length() - 1;
+    while (left < right) {
+        if (str[left] != str[right]) {
+            cout << "\"" << str << "\" is not a palindrome.\n";
+            return 1; 
+        }
+        left++;
+        right--;
+    }
+    cout << "\"" << str << "\" is a palindrome.\n";
+    return 0;
+}
